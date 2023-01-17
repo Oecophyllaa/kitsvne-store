8841510015de2b6da5ac1da7205a85418f6459fd
oecophyllaa <rangga.godzila@gmail.com>
2023-01-18 00:11:52 +0700

    Modif Hero Section | Modif Banner Section | CSS base color

--- src/components/HeroStore.vue
+++ src/components/HeroStore.vue
@@ -6,7 +6,7 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-5">
-              <span>Bag,kids</span>
+              <span>Shirt,teens</span>
               <h1>Black friday</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
               <a href="#" class="primary-btn">Shop Now</a>
@@ -18,7 +18,7 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-5">
-              <span>Bag,kids</span>
+              <span>Sweater,man</span>
               <h1>Black friday</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
               <a href="#" class="primary-btn">Shop Now</a>
@@ -27,6 +27,18 @@
         </div>
       </div>
       <div class="single-hero-items set-bg" style="background-image: url('img/hero-3.jpg')">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5">
+              <span>Bag,kids</span>
+              <h1>Black friday</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+              <a href="#" class="primary-btn">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="single-hero-items set-bg" style="background-image: url('img/hero-4.jpg')">
         <div class="container">
           <div class="row">
             <div class="col-lg-5">
