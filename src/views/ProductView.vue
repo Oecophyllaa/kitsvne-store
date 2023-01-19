f821a810f4a467bb3726a41519bf871ff5493851
Oecophyllaa <rangga.godzila@gmail.com>
2023-01-19 22:47:22 +0700

    Create ProductView and router

---
+++ src/views/ProductView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="product">
+    <HeaderStore />
+    <h1>Halaman Produk</h1>
+    <FooterStore />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderStore from "@/components/HeaderStore.vue";
+import FooterStore from "@/components/FooterStore.vue";
+
+export default {
+  name: "ProductView",
+  components: {
+    HeaderStore,
+    FooterStore,
+  },
+};
+</script>
